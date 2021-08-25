e76cd5e57bfbb61d3b1f492492e2246593b2625b
清水俊樹 <tamreff3290@gmail.com>
2021-08-25 22:40:45 +0900

    step4

--- study/tcpserver.py
+++ study/tcpserver.py
@@ -17,7 +17,7 @@ class TCPServer:
             # wait for a connection from the outside and estabish a connection if there is a connection
             print("Wait for a connection")
             (client_socket, address) = server_socket.accept()
-            print("The connection with the client is complete")
+            print("The connection with the client is complete. remote_address: {}".format(address))
 
             # get the data sent from the client
             request = client_socket.recv(4096)
@@ -26,8 +26,16 @@ class TCPServer:
             with open("server_recv.txt", "wb") as f:
                 f.write(request)
 
+            # get the response data to be sent to the client from the file
+            with open("server_send.txt", "rb") as f:
+                response = f.read()
+
+            # send a response to the client
+            client_socket.send(response)
+
             # the connection is terminated
             client_socket.close()
+
         finally:
             print("Stop the server")
 
