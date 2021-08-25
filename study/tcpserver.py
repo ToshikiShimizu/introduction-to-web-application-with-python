0346c943fb5834b86972727723bf30ba4971e819
清水俊樹 <tamreff3290@gmail.com>
2021-08-25 20:06:27 +0900

    step1

---
+++ study/tcpserver.py
@@ -0,0 +1,37 @@
+import socket
+
+
+class TCPServer:
+    def serve(self):
+        print("Start the server.")
+        try:
+            # create socket
+            server_socket = socket.socket()
+            server_socket.setsockopt(
+                socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+            # assign socket to localhost port 8080
+            server_socket.bind(("localhost", 8080))
+            server_socket.listen(10)
+
+            # wait for a connection from the outside and estabish a connection if there is a connection
+            print("Wait for a connection")
+            (client_socket, address) = server_socket.accept()
+            print("The connection with the client is complete")
+
+            # get the data sent from the client
+            request = client_socket.recv(4096)
+
+            # write the data sent from the client to a file
+            with open("server_recv.txt", "wb") as f:
+                f.write(request)
+
+            # the connection is terminated
+            client_socket.close()
+        finally:
+            print("Stop the server")
+
+
+if __name__ == '__main__':
+    server = TCPServer()
+    server.serve()
