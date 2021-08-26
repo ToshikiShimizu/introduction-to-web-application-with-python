4bdf2a1ff7bdb337e561449eb78441c5d33b7c47
清水俊樹 <tamreff3290@gmail.com>
2021-08-26 14:35:35 +0900

    ch11

--- study/webserver.py
+++ study/webserver.py
@@ -1,7 +1,12 @@
+import os
 import socket
 from datetime import datetime
 
+
 class WebServer:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
     def serve(self):
         print("Start the server.")
         try:
@@ -17,7 +22,8 @@ class WebServer:
             # wait for a connection from the outside and estabish a connection if there is a connection
             print("Wait for a connection")
             (client_socket, address) = server_socket.accept()
-            print("The connection with the client is complete. remote_address: {}".format(address))
+            print("The connection with the client is complete. remote_address: {}".format(
+                address))
 
             # get the data sent from the client
             request = client_socket.recv(4096)
@@ -26,8 +32,33 @@ class WebServer:
             with open("server_recv.txt", "wb") as f:
                 f.write(request)
 
-            # generate response body
-            response_body = "<html><body><h1>It works!</h1></body></html>"
+            # parse the entire request
+            request_line, remain = request.split(b"\r\n", maxsplit=1)
+            request_header, request_body = remain.split(
+                b"\r\n\r\n", maxsplit=1)
+
+            # parse the request line
+            method, path, http_version = request_line.decode().split(" ")
+
+            # delete / at the beginning of path and make it a relative path
+            relative_path = path.lstrip("/")
+
+            # get the path of the file
+            static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
+
+            # debug
+            print(path)
+            print(relative_path)
+            print(static_file_path)
+
+            # generate a response body from a file
+            try:
+                with open(static_file_path, "rb") as f:
+                    response_body = f.read()
+            except OSError:
+                # returns a 404 if the file is not found
+                response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+                response_line = "HTTP/1.1 404 Not Found\r\n"
 
             # generate response line
             response_line = "HTTP/1.1 200 OK\r\n"
@@ -36,12 +67,13 @@ class WebServer:
             response_header = ""
             response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
             response_header += "Host: HenaServer/0.1\r\n"
-            response_header += f"Content-Length: {len(response_body.encode())}\r\n"
+            response_header += f"Content-Length: {len(response_body)}\r\n"
             response_header += "Connection: Close\r\n"
             response_header += "Content-Type: text/html\r\n"
 
             # after attaching the header and body with a blank line, convert it to bytes and generate the entire response
-            response = (response_line + response_header + "\r\n" + response_body).encode()
+            response = (response_line + response_header +
+                        "\r\n").encode() + response_body
 
             # send a response to the client
             client_socket.send(response)
